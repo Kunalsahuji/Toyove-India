abe6908acee45c072c9faa277321005ae74773c9
Kunalsahuji <ksahu0103@gmail.com>
2026-04-09 00:56:27 +0530

    Deployment Ready

--- src/data/brand.js
+++ src/data/brand.js
@@ -5,7 +5,7 @@ export const brand = {
   newsletterCopy: 'Yes! Send me exclusive offers, unique gift ideas, and personalised tips for shopping and selling on Toyove India.',
   poweredByCopy: 'Toyove India is powered by 100% renewable electricity.',
   copyright: '© 2026 Toyove India',
-  footerColumns: [
+  footerColumns: [ 
     { title: 'Shop', links: ['Gift cards', 'Toyove Registry', 'Sitemap', 'Toyove blog', 'Toyove United Kingdom', 'Toyove Germany', 'Toyove Canada'] },
     { title: 'Sell', links: ['Sell on Toyove', 'Teams', 'Forums', 'Affiliates & Creators'] },
     { title: 'About', links: ['Toyove India, Inc.', 'Policies', 'Investors', 'Careers', 'Press', 'Impact', 'Legal imprint'] },
